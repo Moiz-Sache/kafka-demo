437f394c5dbe6b67134ca931d704d6204d319823
Moiz-Sache <moizsache@gmail.com>
2024-03-03 17:11:32 +0530

    initial project commit

---
+++ stock-service/src/test/java/com/mo/stockservice/StockServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mo.stockservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
